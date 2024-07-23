394a4dd56e1afb369e3187d255d8f4a7420e94c2
Harpinder Sandhu <harpindersandhu18@Gmail.com>
2024-07-22 19:07:28 -0600

    DOM

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function testSubmit(event) {
   var username = document.forms["myForm"]["username"].value;
   console.log("username: ", username);
+
+  //   Insert username
+  document.getElementById("results").innerHTML =
+    "<p class='p1'>Username: " + username + "</p>";
+  results.style.backgroundColor = "lightblue";
+  results.style.fontSize = "50px";
+
+  var p1 = document.getElementsByClassName("p1");
+  p1[0].classList.add("p2");
+
   // Prevent default form submission to avoid reloading
   event.preventDefault();
 }
